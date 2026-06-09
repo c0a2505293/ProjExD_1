f93e7323cb0a52db1ccc0e80544e9944b36aae84
ogawa <c0a2505293@edu.teu.ac.jp>
2026-06-09 16:30:18 +0900

    練習7と8と9:背景残像の処理
    git commit -m 練習7と8と9:背景残像の処理

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_flipped = pg.transform.flip(bg_img, True, False)#練8
     kk_img = pg.image.load("fig/3.png")#練3
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -17,12 +18,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        x = x % 3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(kk_img, [300, 200])#練4
+        screen.blit(bg_img, [-x+1600, 0])#練7
+        screen.blit(bg_img_flipped, [-x+3200,0])#練9
+        screen.blit(kk_img, kk_rect)#練4
+        
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200)#練6
+        
 
 
 if __name__ == "__main__":
