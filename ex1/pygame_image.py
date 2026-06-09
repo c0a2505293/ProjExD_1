9fb97e2e6efa7cc9c2ceef15889f1ab4ca3b171c
ogawa <c0a2505293@edu.teu.ac.jp>
2026-06-09 17:23:26 +0900

    演習課題1:キーを押しているときの動きの処理

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -10,37 +10,42 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img_flipped = pg.transform.flip(bg_img, True, False)#練8
-    kk_img = pg.image.load("fig/3.png")#練3
-    kk_img = pg.transform.flip(kk_img, True, False)
-    kk_rect = kk_img.get_rect()#練10-1
-    kk_rect.center = (300, 200)#練10-2
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8：背景画像の左右反転
+    kk_img = pg.image.load("fig/3.png")  # 練習3：こうかとんSurfaceの作成
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習3：こうかとんの左右反転
+    kk_rct = kk_img.get_rect()  # 練習10-1：こうかとんRectの取得
+    kk_rct.center = 300, 200  # 練習10-2：こうかとんRectに中心座標を設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        key_list = pg.key.get_pressed()#練10-3
-        if key_list[pg.K_UP]:
-            kk_rect.move_ip(0, -1)
-        if key_list[pg.K_DOWN]:
-            kk_rect.move_ip(0, +1)
-        if key_list[pg.K_LEFT]:
-            kk_rect.move_ip(-1, 0)
-        if key_list[pg.K_RIGHT]:
-            kk_rect.move_ip(+1, 0)
-        screen.blit(kk_img, kk_rect)
-        x = tmr #練5
-        x = x % 3200
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])#練7
-        screen.blit(bg_img_flipped, [-x+3200,0])#練9
-        screen.blit(kk_img, kk_rect)#練4
-        
+        key_lst = pg.key.get_pressed()  # 練習10-3：すべてのキーの押下状態の取得        
+        key_lst = pg.key.get_pressed()  # キー入力の状態を取得
+        dx, dy = 0, 0
+        bs = 2  # 背景スクロール速度
+
+        # ① 何もキーを押していない場合 → 左へ流れる
+        if not (key_lst[pg.K_UP] or key_lst[pg.K_DOWN] or key_lst[pg.K_LEFT] or key_lst[pg.K_RIGHT]):
+            kk_rct.move_ip(-1, 0)  # 左へ移動
+        # print(key_lst)
+        if key_lst[pg.K_UP]: 
+            kk_rct.move_ip(-1, -1)  
+        if key_lst[pg.K_DOWN]:  
+            kk_rct.move_ip(-1, +1)  
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
+
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])  # 練習5：背景画像を右から左へ
+        screen.blit(bg_img2, [-x+1600, 0])  # 練習7：2枚目Surface
+        screen.blit(bg_img, [-x+3200, 0])  # 練習9：3枚目の背景画像の描画
+        screen.blit(kk_img, kk_rct)  # 練習4：こうかとんSurfaceの描画
         pg.display.update()
         tmr += 1        
-        clock.tick(200)#練6
-        
+        clock.tick(200)
 
 
 if __name__ == "__main__":
